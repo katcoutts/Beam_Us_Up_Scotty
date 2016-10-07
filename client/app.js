3053ff4f2b3ed6225734c7d944f69468b7778b4c
SDouglas92 <simon.douglas2403@sky.com>
2016-10-07 12:18:24 +0100

    made map into global variable within app to work with other functions

--- client/app.js
+++ client/app.js
@@ -1,10 +1,11 @@
 var Map = require("./src/models/map");
+var map;
 
 var app = function(){
   var container = document.getElementById("welcome_map");
   var centre = {lat:0, lng:0};
   var zoom = 2;
-  var map = new Map( container, centre, zoom );
+  map = new Map( container, centre, zoom );
 
   map.createMarker();
   map.addClickListener();
