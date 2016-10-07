de67d5cc24d94c9f4691d8c7f6cbc5bc27b2e52a
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-10-07 11:06:37 +0100

    seeds data working getting into database

--- seeds.js
+++ seeds.js
@@ -3,12 +3,7 @@ use beam_us_up;
 db.diary.insert([
   { 
     title: "Busy day in space",
-    text: "Fixing stuff. Flying about.",
-    date: "01.09.16" 
-  },
-  { 
-    title: "Quiet day in space",
-    text: "Had a nap.",
-    date: "02.09.16" 
-  }];
+    date: "01.09.16",
+    text: "Fixing stuff. Flying about."
+  }]
 );
