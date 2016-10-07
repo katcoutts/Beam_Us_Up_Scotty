0a344c6924e5ca2fe6a122bf434229b5e6066f14
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-10-07 10:47:57 +0100

    made seeds data

---
+++ seeds.js
@@ -0,0 +1,14 @@
+use beam_us_up;
+
+db.diary.insert([
+  { 
+    title: "Busy day in space",
+    text: "Fixing stuff. Flying about.",
+    date: "01.09.16" 
+  },
+  { 
+    title: "Quiet day in space",
+    text: "Had a nap.",
+    date: "02.09.16" 
+  }];
+);
