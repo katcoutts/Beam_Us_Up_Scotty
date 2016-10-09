c8d8f7426a907469681e374d987dba4f2c6ed4e7
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-10-09 15:41:00 +0100

    text above maps updates with each click to tell you when you will pass over that point

--- api/issPassOverApi.js
+++ api/issPassOverApi.js
@@ -1,7 +1,10 @@
 var IssPassOverApi = function(){} 
 
+var myDate;
+var time; 
+
 IssPassOverApi.prototype = {
-  makeRequest: function(position){
+  makeRequest: function(position, marker){
 // GET LAT AND LONG FROM ON EVENT CLICK LISTENER AND THE PUT THEM INTO THE URL AND MAKE THE REQUEST.
 
 
@@ -15,10 +18,15 @@ IssPassOverApi.prototype = {
       json = this.responseText;
       var parsedJson = JSON.parse(json);
       console.log(parsedJson);
-      var time = (parsedJson.response[0].risetime) * 1000;
+      // var requestTime = (parsedJson.request.datetime) * 1000;
+      time = (parsedJson.response[0].risetime) * 1000;
+      // var timeToPoint = (time - requestTime);
+      // console.log("Time to Point" + timeToPoint);
       console.log(time);
-      var myDate = new Date(time);
+      myDate = new Date(time);
       console.log(myDate);
+      var text = document.querySelector('#passover_time');
+      text.innerText = "You will pass over this point on Earth at approximately "+ myDate;
 
 // NEED TO THEN WORK OUT HOW TO TURN THE INFO INTO A PROPER DATE FOR THE NEXT PASSING TIME.
 // var myDate = new Date(1476009011000);
