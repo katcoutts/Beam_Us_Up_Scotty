cb502d97df47e5c5e2c88e35d80b210b45dab395
SDouglas92 <simon.douglas2403@sky.com>
2016-10-12 09:59:30 +0100

    added key constructor to nyt api and abstract key away

--- api/new_york_times_api.js
+++ api/new_york_times_api.js
@@ -1,3 +1,6 @@
+var Key = require('../client/src/models/keys');
+var key = new Key();
+
 var FaveNews = require('../client/src/models/faveNews');
 var faveNews = new FaveNews();
 
@@ -9,7 +12,7 @@ var NYTApi = function(){
 NYTApi.prototype = {
   makeRequest: function(keyword, callback){
     var self = this;
-    var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+keyword+"&page=2&sort=newest&api-key=61724d22315245a79f7e5b3856dc562f";
+    var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+keyword+"&page=2&sort=newest&api-key="+key.nytKey+"";
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.onload = function(){
